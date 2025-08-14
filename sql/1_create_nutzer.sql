e6870a10efd65f348f46e67ab4d4d9ac5c3af6b2
Kaan240705 <as.kaan205@gmail.com>
2025-08-14 09:22:41 +0200

    Update 1_create_nutzer.sql
    Anpassung der Variablen

--- sql/1_create_nutzer.sql
+++ sql/1_create_nutzer.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    vorname VARCHAR(50) NOT NULL,
-    nachname VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
     passwort VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
