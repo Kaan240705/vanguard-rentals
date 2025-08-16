e0b9a85fa50b17d1fd587f268ca4fb96357db16c
Kaan240705 <as.kaan205@gmail.com>
2025-08-16 13:20:24 +0200

    Update produkt.php

--- public/produkt.php
+++ public/produkt.php
@@ -6,7 +6,7 @@ echo "<h2>Verfügbare Produkte</h2>";
 $result = $conn->query("SELECT * FROM produkte");
 if ($result && $result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        echo "<div><strong>{$row['name']}</strong> – {$row['beschreibung']} ({$row['preis']} €)</div>";
+        echo "<div><strong>{$row['name']}</strong> – {$row['describtion']} ({$row['price']} €)</div>";
     }
 } else {
     echo "<p>Keine Produkte verfügbar.</p>";
