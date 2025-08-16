67972ebd69004c615579d31a739d16979e96bacc
Kaan240705 <as.kaan205@gmail.com>
2025-08-16 13:22:50 +0200

    Update produkt.php

--- public/produkt.php
+++ public/produkt.php
@@ -3,7 +3,7 @@ session_start();
 include '../lib/db.php';
 include '../components/header.php';
 echo "<h2>Verfügbare Produkte</h2>";
-$result = $conn->query("SELECT * FROM produkte");
+$result = $conn->query("SELECT * FROM products");
 if ($result && $result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         echo "<div><strong>{$row['name']}</strong> – {$row['describtion']} ({$row['price']} €)</div>";
