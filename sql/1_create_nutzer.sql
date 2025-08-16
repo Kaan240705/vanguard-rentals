8e334577c27f5ab7d10413e0c367b357a6f26d23
Kaan240705 <as.kaan205@gmail.com>
2025-08-16 13:16:12 +0200

    Update 1_create_nutzer.sql

--- sql/1_create_nutzer.sql
+++ sql/1_create_nutzer.sql
@@ -3,6 +3,6 @@ CREATE TABLE users (
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    passwort VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
