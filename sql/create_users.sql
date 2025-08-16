b08843bdf70ffdee3d5608604b3c88bde8cea3ba
Kaan240705 <as.kaan205@gmail.com>
2025-08-16 13:12:38 +0200

    Update create_users.sql

--- sql/create_users.sql
+++ sql/create_users.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL,
     passwort VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
